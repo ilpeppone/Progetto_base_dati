dd08387b056061e67359fb4df70f2afcfcaaa4c7
il_peppone <pepperusso07@gmail.com>
2024-06-29 01:16:12 +0200

    rename 2 files and update 1 file

--- pandas/pulizia_dati.py
+++ pandas/pulizia_dati.py
@@ -2,17 +2,16 @@ import pandas as pd
 
 df_artisti = pd.read_csv("/home/peppe/Progetto_base_dati/pandas/artist_data.csv")#creaiamo il dataframe df_artisti importando il csv artisti
 
-# Converti le colonne nell'ordine corretto
+#conversione delle colonne nell'ordine corretto, nel tipo di dato appropriato, riem
 df_artisti['id'] = pd.to_numeric(df_artisti['id'], downcast='integer')
-df_artisti['name'] = df_artisti['name'].astype(str)
-df_artisti['gender'] = df_artisti['gender'].astype(str)
-df_artisti['dates'] = df_artisti['dates'].astype(str)
-df_artisti['yearOfBirth'] = pd.to_numeric(df_artisti['yearOfBirth'].fillna(pd.NA), downcast='integer')
-df_artisti['yearOfDeath'] = pd.to_numeric(df_artisti['yearOfDeath'].fillna(pd.NA), downcast='integer')
-df_artisti['placeOfBirth'] = df_artisti['placeOfBirth'].astype(str)
-df_artisti['placeOfDeath'] = df_artisti['placeOfDeath'].astype(str)
-df_artisti['url'] = df_artisti['url'].astype(str)
-
+df_artisti['name'] = df_artisti['name'].astype(str).fillna('Unknown')
+df_artisti['gender'] = df_artisti['gender'].astype(str).fillna('Unknown')
+df_artisti['dates'] = df_artisti['dates'].astype(str).fillna('Unknown')
+df_artisti['yearOfBirth'] = pd.to_numeric(df_artisti['yearOfBirth'], errors='coerce').fillna(9999).astype(int)
+df_artisti['yearOfDeath'] = pd.to_numeric(df_artisti['yearOfDeath'], errors='coerce').fillna(9999).astype(int)
+df_artisti['placeOfBirth'] = df_artisti['placeOfBirth'].astype(str).fillna('Unknown')
+df_artisti['placeOfDeath'] = df_artisti['placeOfDeath'].astype(str).fillna('Unknown')
+df_artisti['url'] = df_artisti['url'].astype(str).fillna('Unknown')
 
 print(df_artisti.dtypes)#verifichiamo se interpretati i dati nel dataframe come ci aspettiamo
 print(df_artisti.shape)#verifichiamo quante tuple e attributi ci sono
@@ -22,7 +21,7 @@ df_artisti = df_artisti.drop_duplicates()
 
 print(df_artisti.shape)#verifichiamo quante tuple e attributi ci sono dopo aver eliminato duplicati
 
-df_artisti.to_csv('/home/peppe/Progetto_base_dati/artisti.csv', index=False)
+df_artisti.to_csv('/home/peppe/Progetto_base_dati/artisti_puliti.csv', index=False)
 #########
 
 df_lavori = pd.read_csv("/home/peppe/Progetto_base_dati/pandas/artwork_data.csv")
@@ -36,10 +35,8 @@ df_lavori['title'] = df_lavori['title'].astype(str)
 df_lavori['dateText'] = df_lavori['dateText'].astype(str)
 df_lavori['medium'] = df_lavori['medium'].astype(str)
 df_lavori['creditLine'] = df_lavori['creditLine'].astype(str)
-
-df_lavori['year'] = pd.to_numeric(df_lavori['year'], errors='coerce').fillna(pd.NA).astype(pd.Int64Dtype())
-df_lavori['acquisitionYear'] = pd.to_numeric(df_lavori['acquisitionYear'].fillna(pd.NA), downcast='integer')
-
+df_lavori['year'] = pd.to_numeric(df_lavori['year'], errors='coerce').fillna(9999).astype(pd.Int64Dtype())
+df_lavori['acquisitionYear'] = pd.to_numeric(df_lavori['acquisitionYear'].fillna(9999), downcast='integer')
 df_lavori['dimensions'] = df_lavori['dimensions'].astype(str)
 df_lavori['width'] = pd.to_numeric(df_lavori['width'], errors='coerce')
 df_lavori['height'] = pd.to_numeric(df_lavori['height'], errors='coerce')
@@ -52,5 +49,5 @@ df_lavori['url'] = df_lavori['url'].astype(str)
 
 print(df_lavori.dtypes)
 print(df_lavori.shape)
-df_lavori.to_csv('/home/peppe/Progetto_base_dati/lavori.csv', index=False)
+df_lavori.to_csv('/home/peppe/Progetto_base_dati/lavori_puliti.csv', index=False)
 
